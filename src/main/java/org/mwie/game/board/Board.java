49c42ff29d0f09ea917156dcbc8866e7f83df244
miwiKiwi <mwierzgala@gmail.com>
2023-07-01 15:18:29 +0200

    Stores added to Board

--- src/main/java/org/mwie/game/board/Board.java
+++ src/main/java/org/mwie/game/board/Board.java
@@ -2,8 +2,10 @@ package org.mwie.game.board;
 
 import org.mwie.game.PlayerNumber;
 import org.mwie.game.board.elements.StandardPit;
+import org.mwie.game.board.elements.Store;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,24 +14,38 @@ public class Board {
     //TODO: board should have two players, 12 standardPits and 2 stores
 
     private List<StandardPit>  pits;
+    private List<Store>  stores;
+
+    private Board(BoardBuilder builder) {
+        this.pits = builder.pits;
+        this.stores = builder.stores;
+    }
 
     public List<StandardPit> getPits() {
         return pits;
     }
 
-    private Board(BoardBuilder builder) {
-        this.pits = builder.pits;
+    public List<Store> getStores() {
+        return stores;
     }
 
     public static class BoardBuilder {
         private List<StandardPit> pits;
-        private int stores;
+        private List<Store> stores;
 
         public BoardBuilder withPits(int numberOfPitsPerPlayer) {
-            LinkedList<StandardPit> pitsOne = buildPitsForPlayer(PlayerNumber.ONE, numberOfPitsPerPlayer);
-            LinkedList<StandardPit> pitsTwo = buildPitsForPlayer(PlayerNumber.TWO, numberOfPitsPerPlayer);
+            var pitsOne = buildPitsForPlayer(PlayerNumber.ONE, numberOfPitsPerPlayer);
+            var pitsTwo = buildPitsForPlayer(PlayerNumber.TWO, numberOfPitsPerPlayer);
             pits = new ArrayList<>(pitsOne);
             pits.addAll(pitsTwo);
+            return this;
+        }
+
+        public BoardBuilder withStores() {
+            stores = new ArrayList<>();
+            var storeOne = new Store(PlayerNumber.ONE);
+            var storeTwo = new Store(PlayerNumber.TWO);
+            stores.addAll(Arrays.asList(storeOne, storeTwo));
             return this;
         }
 
--- src/test/java/org/mwie/game/board/BoardTest.java
+++ src/test/java/org/mwie/game/board/BoardTest.java
@@ -3,6 +3,7 @@ package org.mwie.game.board;
 import org.junit.jupiter.api.Test;
 import org.mwie.game.PlayerNumber;
 import org.mwie.game.board.elements.StandardPit;
+import org.mwie.game.board.elements.Store;
 
 import java.util.stream.Collectors;
 
@@ -16,5 +17,13 @@ class BoardTest {
         var pits = board.getPits().stream().collect(Collectors.groupingBy(StandardPit::getOwner));
         assertEquals(6, pits.get(PlayerNumber.ONE).size());
         assertEquals(6, pits.get(PlayerNumber.TWO).size());
+    }
+
+    @Test
+    void shouldHaveOneStorePerPlayer() {
+        Board board = new Board.BoardBuilder().withStores().build();
+        var stores = board.getStores().stream().collect(Collectors.groupingBy(Store::getOwner));
+        assertEquals(1, stores.get(PlayerNumber.ONE).size());
+        assertEquals(1, stores.get(PlayerNumber.TWO).size());
     }
 }
