7f2deed70f2477e5ae2267f7756881aa75ef02dc
miwiKiwi <mwierzgala@gmail.com>
2023-07-07 11:36:41 +0200

    gameRegistry bean removed

--- src/main/java/org/mwie/game/services/ServicesConfiguration.java
+++ src/main/java/org/mwie/game/services/ServicesConfiguration.java
@@ -1,6 +1,5 @@
 package org.mwie.game.services;
 
-import org.mwie.game.GameRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +9,6 @@ public class ServicesConfiguration {
     @Bean
     public GameService gameService(PlayerService playerService) {
         return new GameServiceImpl(playerService);
-    }
-
-    @Bean
-    public GameRegistry gameRegistry() {
-        return new GameRegistry();
     }
 
     @Bean
